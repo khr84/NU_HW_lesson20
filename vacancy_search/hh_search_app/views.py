ff6d0f4daa2cda0e42bedbd0de90c12e4e71545a
khusnutdinov <roman_kh@ngs.ru>
2023-04-20 23:25:38 +0700

    add templates with form and base template

--- vacancy_search/hh_search_app/views.py
+++ vacancy_search/hh_search_app/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from datetime import datetime, timedelta
+from .forms import HistoryForm
+from .models import Search_vacancy
 
 # Create your views here.
+def main_view(request):
+    pass
+    return render(request, 'hh_search_app/index.html')
+
+def search_view(request):
+    pass
+    return render(request, 'hh_search_app/search.html')
+
+def result_view(request):
+    pass
+    return render(request, 'hh_search_app/result.html')
+
+def history_view(request):
+    if request.method == 'POST':
+        form = HistoryForm(request.POST)
+        if form.is_valid():
+            period = form.cleaned_data['period']
+            hist_period = min(30, int(period))
+            now = datetime.now()
+            date_end = now + timedelta(days=1)
+            date_end = date_end.strftime("%Y.%m.%d")
+            date_beg = now - timedelta(days=hist_period)
+            date_beg = date_beg.strftime("%Y.%m.%d")
+            res_list = Search_vacancy.objects.filter(search_date__gte=date_beg, search_date__lt=date_end)
+            return render(request, 'hh_search_app/history.html', context={'vacancy_list': res_list, 'hide':'hidden'})
+        else:
+            return render(request, 'hh_search_app/history.html', context={'form': form, 'hide_res':'hidden'})
+    else:
+        form = HistoryForm()
+        return render(request, 'hh_search_app/history.html', context={'form':form, 'hide_res':'hidden'})
--- vacancy_search/vacancy_search/urls.py
+++ vacancy_search/vacancy_search/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('hh_search_app.urls', namespace='hh_search'))
 ]
